aad2c07ac04b2d7a45c7088a44de06ec60368a10
zachar-AI <zachskihill@gmail.com>
2026-03-05 19:04:20 -0500

    made it so i can actually read comments by making them less dim

--- lua/config/plugins/colors.lua
+++ lua/config/plugins/colors.lua
@@ -4,5 +4,6 @@ return {
   config = function()
     require("nightfox").setup({ options = { transparent = true } })
     vim.cmd.colorscheme("carbonfox")
+    vim.api.nvim_set_hl(0, "Comment", { fg = "#7a8a9e", italic = true })
   end
 }
